f79fdc5fa00978220cbfe48f02c9970ed9f22b92
turbopope <mbrack@uni-koblenz.de>
2017-01-22 12:12:30 +0100

    Remove unused code

--- index.js
+++ index.js
@@ -4,29 +4,6 @@ const APIUseWalker = require('./APIUseWalker');
 const colors = require('colors');
 
 
-function log_functions(obj) {
-  console.log(
-    Object.getOwnPropertyNames(obj).filter(function(p){
-      return typeof(obj[p]) === 'function';
-    })
-  );
-}
-
-function log_properties(obj) {
-  props = Object.getOwnPropertyNames(obj);
-  props.forEach(function(prop){
-    console.log(prop + " - " + typeof(obj[prop]));
-  });
-}
-
-// console.log("request:")
-// log_properties(require('request'));
-// console.log("\ncrypto:")
-// log_properties(require('crypto'));
-
-
-
-// const source = fs.readFileSync('./func.js', 'utf-8');
 
 function forEachJSFileRecursive(path, callback) {
   if (!path.endsWith('/')) { path += '/' }
@@ -64,12 +41,12 @@ function mergeUses(a, b) {
 }
 
 
+
 const repo = process.argv[2];
 const uses = {};
 
 forEachJSFileRecursive(repo, function(filename, contents) {
   console.log(filename.green);
-  // const contents = fs.readFileSync('./testsrc/func.js', 'utf-8');
   const walker = new APIUseWalker(repo, filename);
   try {
     const ast = esprima.parse(contents, { loc: true, tolerant: true });
@@ -77,11 +54,9 @@ forEachJSFileRecursive(repo, function(filename, contents) {
     console.error('Could not parse!');
     return;
   }
-  // console.dir(ast, { depth: null, colors: true });
 
   walker.handleNode(ast);
   mergeUses(uses, walker._uses);
-  // console.dir(walker._uses, { colors: true });
 });
 
 console.dir(uses, { colors: true });
