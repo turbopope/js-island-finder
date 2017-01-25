2cc60a77697d27b5b09d8ba8cd740edfae3aa8fc
turbopope <mbrack@uni-koblenz.de>
2017-01-25 22:48:15 +0100

    Test uses#mergeUses()

--- test/uses.js
+++ test/uses.js
@@ -16,4 +16,57 @@ describe('uses', function() {
       assert.equal(1, valuesIterator.next().value);
     });
   });
+
+  describe('#mergeUses()', function() {
+    it('should return b if a is empty', function() {
+      let a = new Map();
+      let b = new Map();
+      b.set('fs', new Map());
+      b.get('fs').set('john', 1);
+
+      assert.deepEqual(b, uses.mergeUses(a, b));
+    });
+    it('should return a map with a and b', function() {
+      let a = new Map();
+      a.set('fs', new Map());
+      a.get('fs').set('john', 2);
+      let b = new Map();
+      b.set('crypto', new Map());
+      b.get('crypto').set('jade', 1);
+      let ab = new Map();
+      ab.set('crypto', new Map());
+      ab.get('crypto').set('jade', 2);
+      ab.set('fs', new Map());
+      ab.get('fs').set('john', 1);
+
+      assert.deepEqual(ab, uses.mergeUses(a, b));
+    });
+    it('should return a map with authors from a and b', function() {
+      let a = new Map();
+      a.set('fs', new Map());
+      a.get('fs').set('john', 1);
+      let b = new Map();
+      b.set('fs', new Map());
+      b.get('fs').set('jade', 2);
+      let ab = new Map();
+      ab.set('fs', new Map());
+      ab.get('fs').set('john', 1);
+      ab.get('fs').set('jade', 2);
+
+      assert.deepEqual(ab, uses.mergeUses(a, b));
+    });
+    it('should increment use amounts', function() {
+      let a = new Map();
+      a.set('fs', new Map());
+      a.get('fs').set('john', 1);
+      let b = new Map();
+      b.set('fs', new Map());
+      b.get('fs').set('john', 1);
+      let ab = new Map();
+      ab.set('fs', new Map());
+      ab.get('fs').set('john', 2);
+
+      assert.deepEqual(ab, uses.mergeUses(a, b));
+    });
+  });
 });
