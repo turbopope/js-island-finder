dc95bb6ec4483bbc85fc98c21b19f64b75ed09d0
turbopope <mbrack@uni-koblenz.de>
2017-01-25 22:43:08 +0100

    Restructure uses#sortUses() test

--- test/uses.js
+++ test/uses.js
@@ -3,19 +3,14 @@ const uses = require('../lib/uses');
 
 describe('uses', function() {
 
-  let valuesIterator;
-
-  before(function() {
-    let map = new Map();
-    map.set('jade', 2);
-    map.set('john', 1);
-    map.set('rose', 3);
-    map = uses.sortUses(map);
-    valuesIterator = map.values();
-  });
-
   describe('#sortUses()', function() {
     it('should sort the map in descending order', function() {
+      let map = new Map();
+      map.set('jade', 2);
+      map.set('john', 1);
+      map.set('rose', 3);
+      const valuesIterator = uses.sortUses(map).values();
+
       assert.equal(3, valuesIterator.next().value);
       assert.equal(2, valuesIterator.next().value);
       assert.equal(1, valuesIterator.next().value);
