9b320d924c5837f432ff824e5acf34506e050ba2
mbrack <mbrack@uni-koblenz.de>
2017-03-07 19:41:03 +0100

    Move report logic in lib module

---
+++ report/report.js
@@ -0,0 +1,69 @@
+#!/usr/bin/node
+
+const pug = require('pug');
+const fs = require('fs');
+const Table = require('../lib/Table');
+const copyFile = require('../lib/copyFile');
+const execSync = require('child_process').execSync;
+const path = require('path');
+
+function report(dataDir, outDir) {
+  const domainsFile           = `${process.cwd()}/${dataDir}domains.csv`;
+  const modulesFile           = `${process.cwd()}/${dataDir}modules.csv`;
+  const remapFile             = `${process.cwd()}/${dataDir}remap.json`;
+  const modulesToKeywordsFile = `${process.cwd()}/${dataDir}modulesToKeywords.json`;
+
+  const repoName = dataDir.split(path.sep).slice(-2, -1);
+
+  const domains           = Table.parse(fs.readFileSync(domainsFile,           { encoding: "utf-8" }));
+  const modules           = Table.parse(fs.readFileSync(modulesFile,           { encoding: "utf-8" }));
+  const remap             = JSON.parse (fs.readFileSync(remapFile,             { encoding: "utf-8" }));
+  const modulesToKeywords = JSON.parse (fs.readFileSync(modulesToKeywordsFile, { encoding: "utf-8" }));
+
+  const MODULE_THRESHOLD = 25;
+  const AUTHOR_THRESHOLD = 0;
+
+  function sum(ary) {
+    return ary.reduce((a, b) => a + b, 0);
+  }
+
+  for (let row of domains._rows) {
+    if (sum(domains.getRow(row)) < AUTHOR_THRESHOLD) {
+      domains.removeRow(row);
+    }
+  }
+  for (let col of domains._cols) {
+    if (sum(domains.getCol(col)) < MODULE_THRESHOLD) {
+      domains.removeCol(col);
+    }
+  }
+
+  const heatmap = new Table();
+  for (let col of domains._cols) {
+    let colSum = sum(domains.getCol(col));
+    for (let row of domains._rows) {
+      let fraction = domains.get(row, col) / colSum;
+      let heat = Math.floor(fraction * 9);
+      let percentage = (fraction * 100).toFixed(2) + '%';
+      heatmap.set(row, col, {heat, percentage});
+    }
+  }
+
+  const reportData = {
+    title: repoName,
+    short_title: repoName,
+    domains,
+    remap,
+    modules,
+    heatmap,
+    modulesToKeywords,
+    sum: sum
+  }
+
+  execSync(`mkdir -p ${outDir}`, { cwd: process.cwd(), encoding: 'utf-8' });
+  fs.writeFileSync(`${outDir}report.html`, pug.renderFile(__dirname + '/report.pug', reportData));
+  copyFile(__dirname + '/reveal.js',  `${outDir}reveal.js`);
+  copyFile(__dirname + '/report.css', `${outDir}report.css`);
+}
+
+module.exports = report;
