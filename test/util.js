4de3f13f2b5afffec69a0783fc783b5e5dd4aba0
mbrack <mbrack@uni-koblenz.de>
2017-03-13 17:29:44 +0100

    Test util#chunkArray()

--- test/util.js
+++ test/util.js
@@ -13,4 +13,21 @@ describe('util', function() {
       assert.isFalse(util.isMaximumInArray(1, [1, 2]));
     });
   });
+
+  describe('#chunkArray()', function() {
+    it('chunk an array into chunks', function() {
+      assert.deepEqual(util.chunkArray([0,1,2,3], 2), [[0,1], [2,3]]);
+    });
+    it('have the last chunk be incomplete', function() {
+      assert.deepEqual(util.chunkArray([0,1,2,3,4], 2), [[0,1], [2,3], [4]]);
+      assert.deepEqual(util.chunkArray([0], 2), [[0]]);
+    });
+    it('return an empty when given an empty array', function() {
+      assert.deepEqual(util.chunkArray([], 1), []);
+      assert.deepEqual(util.chunkArray([], 2), []);
+    });
+    it('should throw if chunkSize is 0', function() {
+      assert.throws(function(){util.chunkArray([1,2], 0)}, Error);
+    });
+  });
 });
